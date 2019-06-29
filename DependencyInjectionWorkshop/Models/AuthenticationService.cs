8f89ba1290ef701e8f812645b1046e42543b6bf5
Duty920 <shan81920@gmail.com>
2019-06-29 10:24:08 +0800

    第一次新增需求驗證帳號密碼OTP是否正確

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,54 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+using SlackAPI;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string account, string password, string inputOtp)
+        {
+            var hashPassword = "";
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                hashPassword = connection.Query<string>("spGetUserPassword", new {Id = account},
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            var currentOtp = "";
+
+            var httpClient = new HttpClient() {BaseAddress = new Uri("http://joey.com/")};
+            var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                currentOtp = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{account}");
+            }
+
+            if (inputOtp == currentOtp && hash.ToString() == hashPassword)
+            {
+                return true;
+            }
+            
+            var slackClient = new SlackClient("my Api token");
+            slackClient.PostMessage(r => { }, "mychannel", "message");
+            return false;
+        }
     }
 }
