6573ec53acdbe605d54e7d7d2e17c2f1f66f5450
Duty920 <shan81920@gmail.com>
2019-06-30 13:50:06 +0800

    regist Containers and Use DI to create class

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,32 +1,60 @@
 ﻿using System;
 using System.Security.Policy;
+using Autofac;
 using DependencyInjectionWorkshop.Models;
 
 namespace MyConsole
 {
     class Program
     {
+        private static IContainer _containers;
+
+
         static void Main(string[] args)
         {
-            var otpService = new FakeOtp();
-            var hash = new FakeHash();
-            var profile = new FakeProfile();
-            var failCounter = new FakeFailedCounter();
-            var logger = new ConsoleLogger();
-            var notifier = new FakeSlack();
+            RegistContains();
 
-            var authenticationService = new AuthenticationService(otpService, hash, profile);
-            var failCounterDecorator = new FailCounterDecorator(authenticationService, failCounter, logger);
-            var authentication = new NotifyDecorator(failCounterDecorator, notifier);
+//            var otpService = new FakeOtp();
+//            var hash = new FakeHash();
+//            var profile = new FakeProfile();
+//            var failCounter = new FakeFailedCounter();
+//            var logger = new ConsoleLogger();
+//            var notifier = new FakeSlack();
+//
+//            var authenticationService = new AuthenticationService(otpService, hash, profile);
+//            var failCounterDecorator = new FailCounterDecorator(authenticationService, failCounter, logger);
+//            var authentication = new NotifyDecorator(failCounterDecorator, notifier);
+
+            var authentication = _containers.Resolve<IAuthentication>();
 
             var accountId = "Ted";
             var password = "123456";
-            var inputOtp = "3334567";
+//            var inputOtp = "3334567";
+            var inputOtp = "wrongOtp";
+
 //            var accountId = args[0]; //"Ted";
 //            var password = args[1]; //"9527";
 //            var inputOtp = args[2]; //"3334567";
 
             Console.WriteLine(authentication.Verify(accountId, password, inputOtp));
+        }
+
+        private static void RegistContains()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterType<FakeHash>().As<IHash>();
+            containerBuilder.RegisterType<FakeOtp>().As<IOtpService>();
+            containerBuilder.RegisterType<FakeProfile>().As<IProfile>();
+            containerBuilder.RegisterType<FakeSlack>().As<INotifier>();
+            containerBuilder.RegisterType<FakeFailedCounter>().As<IFailCounter>();
+            containerBuilder.RegisterType<ConsoleLogger>().As<ILogger>();
+
+            containerBuilder.RegisterType<AuthenticationService>().As<IAuthentication>();
+            containerBuilder.RegisterDecorator<NotifyDecorator, IAuthentication>();
+            containerBuilder.RegisterDecorator<FailCounterDecorator, IAuthentication>();
+
+            _containers = containerBuilder.Build();
         }
     }
 
